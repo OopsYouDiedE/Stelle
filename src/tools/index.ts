c90e0e7930aeadcbf0e73b31ef88e8a5c74ed2f5
March 7 <2216646743@qq.com>
2026-05-02 23:21:29 +0800

    feat: implement foundational capability modules, tooling registry, and window interfaces for the Stelle architecture

--- src/tools/index.ts
+++
@@ -1,5 +0,0 @@
-﻿export * from "./types.js";
-export { ToolRegistry } from "./registry.js";
-export { createDefaultToolRegistry } from "./factory.js";
-export type { ToolRegistryDeps } from "./factory.js";
-export { validatePublicHttpUrl } from "./security.js";
--- src/tools/providers/default_tools.ts
+++
@@ -1,8 +0,0 @@
-export type { ToolRegistryDeps } from "./deps.js";
-export { createCoreTools } from "./core_tools.js";
-export { createDiscordTools } from "./discord_tools.js";
-export { createLiveTools } from "./live_tools.js";
-export { createMemoryTools } from "./memory_tools.js";
-export { createSceneTools } from "./scene_tools.js";
-export { createSearchTools } from "./search_tools.js";
-export { createTtsTools } from "./tts_tools.js";
